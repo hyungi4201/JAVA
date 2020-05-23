5ed04844cb1dce18f2f01a6a71c5ee3dae53a922
hyungi4201 <dfy0228@naver.com>
2020-05-23 21:48:23 +0900

    10주차

---
+++ JAVA10/src/JAVA10/STAR.java
@@ -0,0 +1,18 @@
+package JAVA10;
+
+import java.util.Scanner;
+
+public class STAR {
+	public static void main(String args[]) {
+		int a;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("홀수를 입력하세요 : ");
+		a = sc.nextInt();
+		
+		for (int i = 1; i == Math.abs(a);i++) {
+			System.out.println();
+			
+		}
+	}
+}
---
+++ JAVA10/src/JAVA10/tempt.java
@@ -0,0 +1,13 @@
+package JAVA10;
+
+public class tempt {
+	public static void main(String args[]) {
+		int year = 4;
+		
+		if(year%4 == 0 && year%100 != 0 && year%400 == 0){
+			
+		System.out.println("¿±≥‚¿‘¥œ¥Ÿ.");
+		
+			}
+	}
+}
