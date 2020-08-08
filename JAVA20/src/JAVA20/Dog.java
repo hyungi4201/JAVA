5adef345d4fead69b50f9a2c0bd2869722b3ec48
hyungi4201 <dfy0228@naver.com>
2020-08-08 21:46:44 +0900

    JAVA 20주차

--- JAVA20/src/JAVA20/Dog.java
+++ JAVA20/src/JAVA20/Dog.java
@@ -1,5 +1,49 @@
 package JAVA20;
 
 public class Dog {
-
+	private String name;
+	private String bread;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getBread() {
+		return bread;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public void setBread(String b) {
+		bread = b;
+	}
+	
+	public void setAge(int a) {
+		age = a;
+	}
+	
+	public Dog() {
+		name = null;
+		bread = null;
+		age = 0;
+	}
+	
+	public Dog(String name, int age) {
+		this.name = name;
+		this.bread = null;
+		this.age = age;
+	}
+	
+	public Dog(String name, String bread, int age) {
+		this.name = name;
+		this.bread = bread;
+		this.age = age;
+	}
 }
